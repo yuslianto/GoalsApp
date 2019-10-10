5fc60dba2e51d17b96607a043da44347e5643105
yuslianto <yuslianto56@gmail.com>
2019-10-10 15:20:09 +0700

    design project with atomic design

--- App.js
+++ App.js
@@ -1,36 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList, } from 'react-native';
+import { StyleSheet, View, FlatList, } from 'react-native';
 
 import GoalItem from './src/Atom/GoalItem'
+import GoalInput from './src/Molekul/GoalInput'
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
   
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals, 
-      { id: Math.random().toString(), value: enteredGoal }
+      { id: Math.random().toString(), value: goalTitle }
     ]);
   };
 
   return (
 
+    
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
+      <GoalInput 
+        onAddGoal={addGoalHandler}
+      />
       <FlatList
         keyExtractor={(item, index)=> item.id}
         data={courseGoals}
@@ -43,16 +34,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
   },
 });
